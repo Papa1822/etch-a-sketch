95155ace967f11792ab12ee29b485bd2efcbd7e4
Papa1822 <mikepaps1822000@gmail.com>
2022-10-01 00:31:44 +0000

    Add a mouseover event to each square of the board.

--- etch.js
+++ etch.js
@@ -1,6 +1,6 @@
 function populateBoard(size){
     let board = document.querySelector('.board');
-    let squares = document.querySelectorAll('div');
+    let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.remove());
 board.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
 board.style.gridTemplateRows = `repeat(${size}, 1fr)`;
@@ -9,10 +9,12 @@ let amount = size * size;
 
 for(let i=0; i<amount; i++){
     let square = document.createElement('div');
+    square.addEventListener('mouseover', () => {
+        square.style.backgroundColor = 'black'
+    });
     square.style.backgroundColor = 'blue';
     board.appendChild(square);
 }
-
 }
 populateBoard(16);
 
