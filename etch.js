92e8316ae55c871ef556ed59d1e0f47047781c5b
Papa1822 <mikepaps1822000@gmail.com>
2022-10-01 02:10:51 +0000

    Feat-added a function so you click to color and click again if you want to end coloring.

--- etch.js
+++ etch.js
@@ -1,4 +1,7 @@
 let color = 'black';
+
+let click = true;
+
 function populateBoard(size){
     let board = document.querySelector('.board');
     let squares = board.querySelectorAll('div');
@@ -26,11 +29,13 @@ function changeSize(input){
    }
 }
 function colorSquare(){
+   if(click){
     if(color === 'random'){
         this.style.backgroundColor = `hsl(${Math.random() * 360}, 100%, 50%)`;
     }else {
         this.style.backgroundColor = color;
     }
+}
 }
 function changeColor(choice){
     color = choice;
@@ -41,3 +46,6 @@ function resetBoard(){
     let squares = board.querySelectorAll('div');
     squares.forEach((div) => div.style.backgroundColor ='white');
 }
+document.querySelector('body').addEventListener('click', () => {
+    click = !click;
+});
